1cade8272429c6584eeb57acbc43efb3e2f6a55b
pratikjadhav718 <55279467+pratikjadhav718@users.noreply.github.com>
2021-03-20 20:05:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pratik/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.pratik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+    }
+}
